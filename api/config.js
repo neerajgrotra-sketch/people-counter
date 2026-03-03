336315c03e5276310f4a9fd6f8d96e293e9777dc
neerajgrotra-sketch <neerajgrotra@gmail.com>
2026-03-03 15:57:40 -0500

    config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,4 @@
 export default function handler(req, res) {
-  // Only send non-sensitive config to the browser
-  // (Overshoot key is still a key, but this avoids putting it in GitHub.)
   res.status(200).json({
     overshootKey: process.env.OVERSHOOT_API_KEY
   });
